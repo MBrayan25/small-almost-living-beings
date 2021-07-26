800bf1a98f20d3260cf06b6cea65a8904bc92386
MBrayan25 <ginniichimaru@gmail.com>
2021-07-26 05:22:22 +0000

    atualizado

--- system.js
+++ system.js
@@ -20,16 +20,17 @@ function padrao(x,y,dna){
         y: y || random(0,cHeight/10)*pixel - pixel,
         time: 0,
         dna: dna || {
-            visao:4,
-            timeMax: 120,
-            timeMin: 35,
+            lifeMin: 30,
+            visao:5,
+            timeMax: 250,
+            timeMin: 30,
             life: 20,
             size: 10
         }
     }
 }
 
-for(let i = 0;i<100;i++){
+for(let i = 0;i<1000;i++){
    newSeed()
 }
 
@@ -93,11 +94,11 @@ function drawCanvas(){ timeGame += 1/10
             deathChickens.push(chickens[id])
         }else{
 
-            if(chickens[id].time < chickens[id].dna.timeMin){chickens[id].cor = "white"}
+            if(chickens[id].time < chickens[id].dna.timeMin && chickens[id].dna.life < chickens[id].dna.lifeMin){chickens[id].cor = "white"}
             else{chickens[id].cor = "gray"}
 
             draw(chickens[id].x,chickens[id].y,chickens[id].dna.size,chickens[id].dna.size,chickens[id].cor)
-            actionChicken(id)
+            moveChicken(id)
             colid(id)
             if(chickens[id].dna.life < 1){ chickens[id].kill = true }
         }
@@ -117,9 +118,12 @@ function drawCanvas(){ timeGame += 1/10
         }
     }
 
-    for(let seed of seeds){
-        let m =  + ((pixel-pixel/2)/2)
-        draw(seed.x + m,seed.y + m,seed.size,seed.size,"green")
+    for(let id in seeds){
+        let seed = seeds[id]
+        if(seed.x < 0 || seed.y < 0){seeds.splice(id,1)}else{
+            let m = ((pixel-pixel/2)/2)
+            draw(seed.x + m,seed.y + m,seed.size,seed.size,"green")
+        }
     }
 
     if(timeSeeds === 0){newSeed();timeSeeds = time/10}else{timeSeeds -= 1}
@@ -143,40 +147,60 @@ function newSeed(x,y) {
 
 function newChicken(x,y,dna) { chickens.push(padrao(x,y,dna)) }
 
-function actionChicken(id){
-    let seedR = []
-    for(seed of seeds){
-        if(radar(chickens[id],seed)){seedR.push(seed)}
-    }
+function radar(chicken,Seeds){
+    let listSeed = []
+    x1 = chicken.x - chicken.dna.visao*pixel
+    y1 = chicken.y - chicken.dna.visao*pixel
+    x2 = chicken.x + chicken.dna.visao*pixel
+    y2 = chicken.y + chicken.dna.visao*pixel
 
-    let x = iaChicken().x ; y = iaChicken().y
+    for(seed of Seeds){
+        if(seed.x > x1 && seed.x < x2 && seed.y > y1 && seed.y < y2){
+            listSeed.push(seed)
+        } 
+    }
+    return listSeed
+}
+
+function moveChicken(id){
+    let x = 0, y = 0, dire = random(1,100)
+
+    if(dire <= 50){dire = radar(chickens[id],seeds)}else{dire = radar(chickens[id],chickens)}
+
+    let direct = direction(chickens[id],dire)
+
+    if(direct <= 1000){y = -pixel}
+    else if(direct <= 2000){y = pixel}
+    else if(direct <= 3000){x = -pixel}
+    else if(direct <= 4000){x = pixel}
+    else if(direct <= 5000){x = -pixel, y = -pixel}
+    else if(direct <= 6000){x = pixel, y = -pixel}
+    else if(direct <= 7000){x = -pixel, y = pixel}
+    else if(direct <= 8000){x = pixel, y = pixel}
 
     if(chickens[id].x < pixel && x < 0 || chickens[id].x > cWidth - pixel*2 && x > 0){x=0}
     if(chickens[id].y < pixel && y < 0 || chickens[id].y > cHeight - pixel*2 && y > 0){y=0}
 
-    moveChicken(id,x,y)
+    chickens[id].x += x, chickens[id].y += y
 }
 
-function iaChicken(){
-
-    let x = random(-1,1)
-    let y = random(-1,1)
-
-    if(x<0){x= -10}else if(x>0){x=10}else{x=0}
-    if(y<0){y= -10}else if(y>0){y=10}else{y=0}
-    return {x,y}
-}
-
-function moveChicken(id,x,y){ chickens[id].x += x ; chickens[id].y += y }
-
-function radar(chicken,seed){
-    x1 = chicken.x - chicken.dna.visao*pixel
-    y1 = chicken.y - chicken.dna.visao*pixel
-
-    x2 = chicken.x + chicken.dna.visao*pixel
-    y2 = chicken.y + chicken.dna.visao*pixel
-
-    if(seed.x > x1 && seed.x < x2 && seed.y > y1 && seed.y < y2){ return true }else{ return false }
+function direction(chicken,listSeed){
+    if(listSeed.length > 0){
+      let d = []
+        for(Seed of listSeed){
+            d.push(Math.sqrt((Seed.x - chicken.x)**2 + (Seed.y - chicken.y)**2))
+        }
+        let seed = listSeed[d.indexOf(Math.min(...d))]
+        if(seed.y < chicken.y && seed.x < chicken.x){return 5000}
+        if(seed.y < chicken.y && seed.x > chicken.x){return 6000}
+        if(seed.y > chicken.y && seed.x < chicken.x){return 7000}
+        if(seed.y > chicken.y && seed.x > chicken.x){return 8000}
+        if(seed.y < chicken.y){return 1000}
+        if(seed.y > chicken.y){return 2000}
+        if(seed.x < chicken.x){return 3000}
+        if(seed.x > chicken.x){return 4000}
+    }
+    return random(1,8000)
 }
 
 function movePlayer(event){
@@ -209,7 +233,11 @@ function colidSeeds(chicken){
 function colid(idA){
     for(idB in chickens){
         if(idA !== idB){
-            if(chickens[idA].x === chickens[idB].x && chickens[idA].y === chickens[idB].y && chickens[idA].time > chickens[idA].dna.timeMin && chickens[idB].time > chickens[idB].dna.timeMin){
+            let ifA = chickens[idA].x === chickens[idB].x && chickens[idA].y === chickens[idB].y
+            let ifB = chickens[idA].time > chickens[idA].dna.timeMin && chickens[idB].time > chickens[idB].dna.timeMin
+            let ifC = chickens[idA].dna.life > chickens[idA].dna.lifeMin && chickens[idB].dna.life > chickens[idB].dna.lifeMin
+
+            if( ifA && ifB && ifC){
                 newGeration += 1
                 console.log()
                 newChicken(chickens[idA].x,chickens[idA].y)
